9aeccca5efb0b3c8ef9311d8c922bc0276671e56
sxc <qiuyequ@icloud.com>
2020-09-18 09:58:29 +0800

    Initial Commit

---
+++ DesignCodeCourse/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  DesignCodeCourse
+//
+//  Created by Xiaochun Shen on 2020/9/18.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ DesignCodeCourse/DesignCodeCourseApp.swift
@@ -0,0 +1,17 @@
+//
+//  DesignCodeCourseApp.swift
+//  DesignCodeCourse
+//
+//  Created by Xiaochun Shen on 2020/9/18.
+//
+
+import SwiftUI
+
+@main
+struct DesignCodeCourseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
