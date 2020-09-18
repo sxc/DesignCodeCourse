8ee969d79f37e18b1414e4d8e70d9d7b09dfb58c
sxc <qiuyequ@icloud.com>
2020-09-18 10:47:22 +0800

    add pages

--- DesignCodeCourse/ContentView.swift
+++ DesignCodeCourse/ContentView.swift
@@ -9,13 +9,36 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack(alignment: .leading, spacing: 4.0) {
+            Spacer()
+            HStack {
+                    Spacer()
+                    Image("Illustration 1")
+                        .resizable()
+                        .aspectRatio(contentMode: /*@START_MENU_TOKEN@*/.fit/*@END_MENU_TOKEN@*/)
+                    Spacer()
+            }
+            Text("SwiftUI for iOS14").fontWeight(.bold).foregroundColor(/*@START_MENU_TOKEN@*/.white/*@END_MENU_TOKEN@*/)
+            
+            Text("20 Sections").font(.footnote)
+                .foregroundColor(.white)
+        }
+        .padding(.all)
+        .background(Color.blue)
+        .cornerRadius(20.0)
+        .shadow(radius: 10)
+        
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        Group {
+            ContentView()
+            ContentView()
+                .previewLayout(.fixed(width: 200.0, height: 200.0))
+                .preferredColorScheme(.dark)
+            
+        }
     }
 }
